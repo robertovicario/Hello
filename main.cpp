665a59f5517052b721ebbb7cb8e8aaf4a4e7463a
robertovicario <rvicariodev@gmail.com>
2024-03-04 12:14:39 +0100

    updates

--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
-#include <yaml-cpp/yaml.h> // You'll need to install yaml-cpp library. Use `sudo apt-get install libyaml-cpp-dev` on Ubuntu.
+#include <yaml-cpp/yaml.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <name> <filename>" << std::endl;
+        return 1;
+    }
+
+    std::string name = argv[1];
+    std::string filename = argv[2];
+
     try {
         // Load YAML file
-        YAML::Node config = YAML::LoadFile("../config.yaml");
+        YAML::Node config = YAML::LoadFile("../" + filename);
 
-        // Read configuration settings
-        std::string name = config["name"].as<std::string>();
-        int age = config["age"].as<int>();
+        // Search for data of the person with given name
+        YAML::Node personData = config[name];
 
-        // Print configuration settings
-        std::cout << "Name: " << name << std::endl;
-        std::cout << "Age: " << age << std::endl;
+        if (personData) {
+            // Read person's data
+            std::string name = personData["name"].as<std::string>();
+            int age = personData["age"].as<int>();
+
+            // Print person's data
+            std::cout << "Name: " << name << std::endl;
+            std::cout << "Age: " << age << std::endl;
+        } else {
+            std::cerr << "Person with name '" << name << "' not found in the file." << std::endl;
+            return 1;
+        }
     } catch(const YAML::Exception& e) {
         std::cerr << "Error reading YAML file: " << e.what() << std::endl;
         return 1;
